c11adf8bf31a5ad717b84d2e6828a5f4557aacca
balu13 <balu13@gmail.com>
2017-03-25 15:15:44 +0200

    least common multiple fast

--- Algorithmic Toolbox/LeastCommonMultiplier/lcm.cpp
+++ Algorithmic Toolbox/LeastCommonMultiplier/lcm.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <algorithm>
+#include <cassert>
 
 long long lcm_naive(int a, int b) {
   for (long l = 1; l <= (long long) a * b; ++l)
@@ -8,9 +10,43 @@ long long lcm_naive(int a, int b) {
   return (long long) a * b;
 }
 
+int gcd_fast(int a, int b)
+{
+	int currentMultiplier = std::max(a, b);
+	int currentDivisor = std::min(a, b);
+	int current_gcd;
+	while ((current_gcd = currentMultiplier % currentDivisor) != 0)
+	{
+		currentMultiplier = currentDivisor;
+		currentDivisor = current_gcd;
+	}
+
+	return currentDivisor;
+}
+
+unsigned long long lcm_fast(int a, int b)
+{
+	int gcd = gcd_fast(a, b);
+	return  ((long long)a * b) / gcd;
+}
+
+void test_solution(int range)
+{
+	for (int a = 1; a < range; ++a)
+	{
+		for (int b = 1; b < range; ++b)
+		{
+			assert(lcm_fast(a, b) == lcm_fast(a, b));
+		}
+	}
+}
+
 int main() {
   int a, b;
+  //std::cin >>a;
+  //test_solution(a);
   std::cin >> a >> b;
-  std::cout << lcm_naive(a, b) << std::endl;
+  //std::cout << lcm_naive(a, b) << std::endl;
+  std::cout << lcm_fast(a, b) << std::endl;
   return 0;
 }
