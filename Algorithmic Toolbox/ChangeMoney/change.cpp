fd17d44bd0b09ec80587a1f5a330bd90ca97b2e0
balu13 <balu13@gmail.com>
2017-03-30 06:56:52 +0300

    change_money initial tests

--- Algorithmic Toolbox/ChangeMoney/change.cpp
+++ Algorithmic Toolbox/ChangeMoney/change.cpp
@@ -1,12 +1,50 @@
 #include <iostream>
+#include <algorithm>
+#include <cassert>
 
-int get_change(int m) {
-  //write your code here
-  return n;
+int coins[] { 1, 5, 10 };
+
+int get_change(int m)
+{
+	int numberOfCoins{ 0 };
+	return numberOfCoins;
 }
 
-int main() {
-  int m;
-  std::cin >> m;
-  std::cout << get_change(m) << '\n';
+void test1()
+{
+	assert(get_change(2) == 2);
+}
+
+void test2()
+{
+	assert(get_change(28) == 6);
+}
+
+void testEdgeCases()
+{
+}
+
+void testLargeNumbers()
+{
+}
+
+void testRandomValues()
+{
+}
+
+void testSuite()
+{
+	test1();
+	test2();
+	testEdgeCases();
+	testLargeNumbers();
+	testRandomValues();
+}
+
+int main()
+{
+	int m{ 0 };
+	testSuite();
+//	std::cin >> m;
+	std::cout << get_change(m) << '\n';
 }
