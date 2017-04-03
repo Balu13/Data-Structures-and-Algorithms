296a4241fc80876a8134f1d3c32a59840c5f502e
balu13 <balu13@gmail.com>
2017-04-03 07:08:34 +0300

    Max ad revenue - correct but fishy - uses standard sorts

--- Algorithmic Toolbox/MaxAdRevenue/dot_product.cpp
+++ Algorithmic Toolbox/MaxAdRevenue/dot_product.cpp
@@ -4,15 +4,15 @@
 #include <set>
 
 using std::vector;
-using std::set;
+using std::multiset;
 
-long long max_dot_product(set<int> a, set<int> b) {
+long long max_dot_product(multiset<int> a, multiset<int> b) {
   // write your code here
   long long result = 0;
   //for (size_t i = 0; i < a.size(); i++) {
   //  result += ((long long) a[i]) * b[i];
   //}
-  set<int>::const_iterator ita, itb;
+  multiset<int>::const_iterator ita, itb;
   for (ita = a.cbegin(), itb = b.cbegin(); ita != a.cend(); ita++, itb++)
   {
 	  result += ((long long)*ita) * (*itb);
@@ -24,7 +24,7 @@ int main() {
   size_t n;
   std::cin >> n;
   vector<int> a(n), b(n);
-  std::set<int> sa, sb;
+  multiset<int> sa, sb;
   for (size_t i = 0; i < n; i++) {
     std::cin >> a[i];
 	sa.insert(a[i]);
