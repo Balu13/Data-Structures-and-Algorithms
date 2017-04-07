07c4e28c0284ec1be0fb0de42e3033911a40152a
balu13 <balu13@gmail.com>
2017-04-07 06:55:19 +0300

    covering segments solution

--- Algorithmic Toolbox/CollectingSignatures/covering_segments.cpp
+++ Algorithmic Toolbox/CollectingSignatures/covering_segments.cpp
@@ -10,32 +10,39 @@ struct Segment {
 };
 
 vector<int> optimal_points(vector<Segment> &segments) {
-  vector<int> points;
-  vector<Segment> covered;
+	vector<int> points;
 
-  size_t curSegment{ 0 };
-  for (size_t i = 1; i < segments.size(); ++i) {
-	  if (segments[i].end < segments[curSegment].end) {
-		  if (segments[i].end >= segments[curSegment].start) {
-			  covered.push_back(segments[curSegment]);
-		  }
-	  }
-  }
-  covered.push_back(segments[0]);
+	if (segments.empty())
+	{
+		return points;
+	}
 
-  return points;
+	std::sort(segments.begin(), segments.end(), [](Segment s1, Segment s2) { return s1.end < s2.end; });
+
+	int curSegment{ 0 };
+	points.push_back(segments[0].end);
+	for (size_t i = 1; i < segments.size(); ++i)
+	{
+		if (segments[i].start > segments[curSegment].end && segments[i].end > segments[curSegment].end)
+		{
+			points.push_back(segments[i].end);
+			curSegment = i;
+		}
+	}
+
+	return points;
 }
 
 int main() {
-  int n;
-  std::cin >> n;
-  vector<Segment> segments(n);
-  for (size_t i = 0; i < segments.size(); ++i) {
-    std::cin >> segments[i].start >> segments[i].end;
-  }
-  vector<int> points = optimal_points(segments);
-  std::cout << points.size() << "\n";
-  for (size_t i = 0; i < points.size(); ++i) {
-    std::cout << points[i] << " ";
-  }
+	int n;
+	std::cin >> n;
+	vector<Segment> segments(n);
+	for (size_t i = 0; i < segments.size(); ++i) {
+		std::cin >> segments[i].start >> segments[i].end;
+	}
+	vector<int> points = optimal_points(segments);
+	std::cout << points.size() << "\n";
+	for (size_t i = 0; i < points.size(); ++i) {
+		std::cout << points[i] << " ";
+	}
 }
