31f2a5ba7ed81fff12f21cb6003553c4e80196ea
balu13 <balu13@gmail.com>
2017-04-10 08:37:36 +0300

    finding majority element done

--- Algorithmic Toolbox/MajorityElement/majority_element.cpp
+++ Algorithmic Toolbox/MajorityElement/majority_element.cpp
@@ -1,14 +1,31 @@
 #include <algorithm>
 #include <iostream>
 #include <vector>
+#include <map>
 
 using std::vector;
+using std::map;
 
 int get_majority_element(vector<int> &a, int left, int right) {
   if (left == right) return -1;
   if (left + 1 == right) return a[left];
-  //write your code here
-  return -1;
+  
+  map<int, int> sortedCount;
+  int retVal = -1;
+  std::for_each(a.cbegin(), a.cend(), [&](int val)
+  {
+	  std::pair<map<int, int>::iterator, bool> element = sortedCount.emplace(val, 1);
+	  if (!element.second)
+	  {
+		  ++((*(element.first)).second);
+	  }
+	  if (((*(element.first)).second) > a.size()/2)
+	  {
+		  retVal = (*(element.first)).first;
+		  return;
+	  }
+  });
+  return retVal;
 }
 
 int main() {
