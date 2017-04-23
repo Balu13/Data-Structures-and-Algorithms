4e00107d89fd6f51097764a3c030f0d2bdcac78e
balu13 <balu13@gmail.com>
2017-04-23 23:15:55 +0300

    knapsack with repetitions solution

--- Algorithmic Toolbox/TakeGoldKnapsack/knapsack.cpp
+++ Algorithmic Toolbox/TakeGoldKnapsack/knapsack.cpp
@@ -1,20 +1,44 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using std::vector;
 
-int optimal_weight(int W, const vector<int> &w) {
-	//write your code here
-	int current_weight = 0;
-	for (size_t i = 0; i < w.size(); ++i) {
-		if (current_weight + w[i] <= W) {
-			current_weight += w[i];
+int optimal_weight(int W, vector<int> &w)
+{
+	w.insert(w.begin(), 0);
+	vector<vector<int>> matrix = *(new vector<vector<int>>(w.size()));
+	//matrix[0] = *(new vector<int>(W + 1, 0));
+	/*for (int j = 0; j <= W; ++j)
+	{
+		matrix[0][j] = 0;
+	}*/
+	for (size_t i = 0; i < w.size(); ++i)
+	{
+		matrix[i] = *(new vector<int>(W + 1, 0));
+//		matrix[i][0] = 0;
+	}
+
+	for (size_t i = 1; i < w.size(); ++i)
+	{
+		for (int j = 1; j <= W; ++j)
+		{
+			if (w[i] > j)
+			{
+				matrix[i][j] = matrix[i - 1][j];
+			}
+			else
+			{
+				matrix[i][j] = std::max(matrix[i - 1][j], matrix[i - 1][j - w[i]] + w[i]);
+			}
 		}
 	}
-	return current_weight;
+
+	return matrix[w.size() - 1][W];
 }
 
-int main() {
+int main()
+{
 	int n, W;
 	std::cin >> W >> n;
 	vector<int> w(n);
