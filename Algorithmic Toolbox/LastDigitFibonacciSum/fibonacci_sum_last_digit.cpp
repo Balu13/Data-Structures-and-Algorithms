3d51be577dad61ea1d9307ed008c26ef5cc9c3fd
balu13 <balu13@gmail.com>
2017-04-24 01:21:33 +0300

    last digit of n fibos sum solution

--- Algorithmic Toolbox/LastDigitFibonacciSum/fibonacci_sum_last_digit.cpp
+++ Algorithmic Toolbox/LastDigitFibonacciSum/fibonacci_sum_last_digit.cpp
@@ -18,14 +18,28 @@ int fibonacci_sum_naive(long long n) {
     return sum % 10;
 }
 
-int fibonacci_sum_naive(long long n)
+int fibonacci_sum_optimal(long long n)
 {
-	return n % 10;
+	n %= 60;
+	if (n == 0) return 0;
+
+	int previous1 = 0;
+	int previous2 = 1;
+	int lastDigitOfSum = 1;
+	for (long long i = 0; i < n - 1; ++i)
+	{
+		int tmpPrevious = previous1;
+		previous1 = previous2;
+		previous2 = (tmpPrevious + previous1) % 10;
+		lastDigitOfSum = (lastDigitOfSum + previous2) % 10;
+		//std::cout << lastDigitOfSum << " ";
+	}
+	return lastDigitOfSum;
 }
 
 int main() {
     long long n = 0;
     std::cin >> n;
     //std::cout << fibonacci_sum_naive(n) << '\n';
-	std::cout << fibonacci_sum_naive(n) << '\n';
+	std::cout << fibonacci_sum_optimal(n) << '\n';
 }
