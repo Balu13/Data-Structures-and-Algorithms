302c9e131618165afb7acff6a488b9396ce359b8
balu13 <balu13@gmail.com>
2017-05-19 08:21:33 +0300

    Parallel processing solution

--- Data Structures/ParallelProcessing/job_queue.cpp
+++ Data Structures/ParallelProcessing/job_queue.cpp
@@ -34,22 +34,138 @@ private:
 
 	void AssignJobs()
 	{
-		// TODO: replace this code with a faster algorithm.
 		assigned_workers_.resize(jobs_.size());
 		start_times_.resize(jobs_.size());
 		vector<long long> next_free_time(num_workers_, 0);
+		vector<int> heap(num_workers_, -1);
+		int currentUnusedWorker = 0;
+
 		for (int i = 0; i < jobs_.size(); ++i)
 		{
 			int duration = jobs_[i];
-			int next_worker = 0;
-			for (int j = 0; j < num_workers_; ++j)
+			if (currentUnusedWorker < num_workers_)
 			{
-				if (next_free_time[j] < next_free_time[next_worker])
-					next_worker = j;
+				insert(currentUnusedWorker, heap, i, duration, next_free_time);
 			}
-			assigned_workers_[i] = next_worker;
-			start_times_[i] = next_free_time[next_worker];
-			next_free_time[next_worker] += duration;
+			else
+			{
+				// change thread priority in the heap
+				feedNextWorker(heap, i, duration, next_free_time);
+			}
+		}
+	}
+
+	void insert(int& currentUnusedWorker, vector<int>& heap, int jobNo, int jobDuration, vector<long long>& next_free_time)
+	{
+		assigned_workers_[jobNo] = currentUnusedWorker;
+		start_times_[jobNo] = next_free_time[currentUnusedWorker];
+		next_free_time[currentUnusedWorker] += jobDuration;
+		heap[currentUnusedWorker] = currentUnusedWorker;
+		int currentPos = currentUnusedWorker++;
+		//liftUp()
+		while (currentPos != 0 && next_free_time[heap[currentPos]] < next_free_time[heap[(currentPos - 1) / 2]])
+		{
+			std::swap(heap[currentPos], heap[(currentPos - 1) / 2]);
+			currentPos = (currentPos - 1) / 2;
+		}
+	}
+
+	void feedNextWorker(vector<int>& heap, int jobNo, int jobDuration, vector<long long>& next_free_time)
+	{
+		assigned_workers_[jobNo] = heap[0];
+		start_times_[jobNo] = next_free_time[heap[0]];
+		next_free_time[heap[0]] += jobDuration;
+		int currentPos{ 0 };
+		//siftDown()
+		while (currentPos * 2 + 1 < heap.size())
+		{
+			if (currentPos * 2 + 2 < heap.size())
+			{
+				if (next_free_time[heap[currentPos * 2 + 1]] > next_free_time[heap[currentPos * 2 + 2]])
+				{
+					if (next_free_time[heap[currentPos]] > next_free_time[heap[currentPos * 2 + 2]] ||
+						next_free_time[heap[currentPos]] == next_free_time[heap[currentPos * 2 + 2]] && heap[currentPos] > heap[currentPos * 2 + 2])
+					{
+						std::swap(heap[currentPos], heap[currentPos * 2 + 2]);
+						currentPos = currentPos * 2 + 2;
+					}
+					else
+					{
+						break;
+					}
+				}
+				else if (next_free_time[heap[currentPos * 2 + 1]] == next_free_time[heap[currentPos * 2 + 2]])
+				{
+					if (next_free_time[heap[currentPos]] > next_free_time[heap[currentPos * 2 + 1]])
+					{
+						if (heap[currentPos * 2 + 1] > heap[currentPos * 2 + 2])
+						{
+							std::swap(heap[currentPos], heap[currentPos * 2 + 2]);
+							currentPos = currentPos * 2 + 2;
+						}
+						else
+						{
+							std::swap(heap[currentPos], heap[currentPos * 2 + 1]);
+							currentPos = currentPos * 2 + 1;
+						}
+					}
+					else if (next_free_time[heap[currentPos]] == next_free_time[heap[currentPos * 2 + 1]])
+					{
+						if (heap[currentPos] > heap[currentPos * 2 + 1])
+						{
+							if (heap[currentPos * 2 + 1] > heap[currentPos * 2 + 2])
+							{
+								std::swap(heap[currentPos], heap[currentPos * 2 + 2]);
+								currentPos = currentPos * 2 + 2;
+							}
+							else
+							{
+								std::swap(heap[currentPos], heap[currentPos * 2 + 1]);
+								currentPos = currentPos * 2 + 1;
+							}
+						}
+						else if (heap[currentPos] > heap[currentPos * 2 + 2])
+						{
+							std::swap(heap[currentPos], heap[currentPos * 2 + 2]);
+							currentPos = currentPos * 2 + 2;
+						}
+						else
+						{
+							break;
+						}
+					}
+					else
+					{
+						break;
+					}
+				}
+				else
+				{
+					if (next_free_time[heap[currentPos]] > next_free_time[heap[currentPos * 2 + 1]] ||
+						next_free_time[heap[currentPos]] == next_free_time[heap[currentPos * 2 + 1]] && heap[currentPos] > heap[currentPos * 2 + 1])
+					{
+						std::swap(heap[currentPos], heap[currentPos * 2 + 1]);
+						currentPos = currentPos * 2 + 1;
+					}
+					else
+					{
+						break;
+					}
+				}
+			}
+			else
+			{
+				if (next_free_time[heap[currentPos]] > next_free_time[heap[currentPos * 2 + 1]] ||
+					next_free_time[heap[currentPos]] == next_free_time[heap[currentPos * 2 + 1]] && heap[currentPos] > heap[currentPos * 2 + 1])
+				{
+					std::swap(heap[currentPos], heap[currentPos * 2 + 1]);
+					currentPos = currentPos * 2 + 1;
+				}
+				else
+				{
+					break;
+				}
+			}
 		}
 	}
 
